ce34d3a0c31b088ebab1c0c3e11b2681c65dd506
William <william@websnack.dk>
2021-06-17 09:55:27 +0200

    Testing hooks command

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#
-# Automatically configures mutagen sync for the current .ddev project.
-#
-
 echo "=> Checking ddev project"
 if [ ! -d ".ddev" ]; then
   echo "[!] This does not appear to be a ddev project."
@@ -27,45 +23,14 @@ echo "     https://raw.githubusercontent.com/williamengbjerg/ddev-mutagen/master
 if [ -f ".ddev/commands/host/mutagen" ]; then
     rm -rf .ddev/commands/host/mutagen
 fi
+if [ -f ".ddev/config.mutagen.yaml" ]; then
+    rm -rf .ddev/config.mutagen.yaml
+fi
 
+curl -s https://raw.githubusercontent.com/williamengbjerg/ddev-mutagen/master/config.mutagen.yaml > .ddev/config.mutagen.yaml
 curl -s https://raw.githubusercontent.com/williamengbjerg/ddev-mutagen/master/mutagen > .ddev/commands/host/mutagen
 mkdir -p .ddev/project-stopped
 chmod +x .ddev/commands/host/mutagen
-
-# If hooks are already present, let's not break their config.
-if cat .ddev/config.yaml | grep "^hooks:" > /dev/null; then
-  echo "=> It looks like you already have active hooks in your .ddev/config.yaml."
-  echo "   To finish setup, you'll need to add/merge the following config into"
-  echo "   what you already have configured:"
-  echo
-  echo
-  cat << EOF
-no_project_mount: true
-hooks:
-  post-start:
-    # Start the mutagen sync process for this project.
-    - exec-host: "ddev mutagen start"
-  pre-stop:
-    # Terminate the mutagen sync process for this project.
-    - exec-host: "ddev mutagen stop
-EOF
-  echo
-  echo
-# Otherwise, we can safely just tack this onto the end of the .ddev/config.yaml and
-# we're good to go.
-else
-  echo "=> Setting up .ddev/config.yaml to run the mutagen sync script"
-  cat >> .ddev/config.yaml <<'config'
-no_project_mount: true
-hooks:
-  post-start:
-    # Start the mutagen sync process for this project.
-    - exec-host: "ddev mutagen start"
-  pre-stop:
-    # Terminate the mutagen sync process for this project.
-    - exec-host: "ddev mutagen stop"
-config
-fi
 
 echo
 echo "All set! Run 'ddev start' to start using mutagen."
